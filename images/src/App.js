36af2b272a1233e5eb97c78e2c2776c8b521fc33
Mariantonieta <mariantonietachacondibacco@gmail.com>
2022-11-28 20:22:26 +0100

    first commit

---
+++ images/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Slider from './components/Slider';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Carousel</h1>
+    <Slider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ images/src/components/Slider.jsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import images from './images'
+const Slider = () => {
+const [width, setWidth] = useState(0)
+const carousel = useRef()
+useEffect(() =>{
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth)
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+}, [])
+return (<>
+<motion.div ref={carousel} className="carousel" whileTap={{cursor: "grabbing"}}>
+    <motion.div  drag="x" dragConstraints={{right:0, left: -width}}  className="inner-carousel"> 
+    {images.map(image =>{
+        return (
+            <motion.div className="item" key={image}> <img src={image} alt="" /> </motion.div>
+        )
+    } )}
+    </motion.div>
+</motion.div>
+
+</>)
+}
+export default Slider
